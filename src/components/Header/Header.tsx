104729a3afd5645cff596df4ecd07b091520c2fa
Luís Azevedo <luismiguelgomes2001@gmail.com>
2022-03-06 23:14:05 -0100

    ADD: Sass / Header.styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,9 @@
 import profileImage from "../../assets/pfp.gif";
+import "./Header.styles.scss";
 
 const Header = () => {
   return (
-    <div>
+    <div className="header">
       <img src={profileImage} width="80px" alt="Profile picture" />
     </div>
   );
