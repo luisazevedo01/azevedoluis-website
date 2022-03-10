a6ef0ea5e37d098540005da80a724ffa5b24000b
Luís Azevedo <luismiguelgomes2001@gmail.com>
2022-03-10 18:10:37 -0100

    ADD: personal_picture / FIX: Home.styles

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,4 +1,5 @@
 import Header from "../../components/Header/Header";
+import personalPicture from "../../assets/personal_picture.jpeg";
 import "./Home.styles.scss";
 
 //TODO: add picture, description and style
@@ -6,15 +7,19 @@ const Home = () => {
   return (
     <div className="home">
       <Header />
-      <section className="home_picture"></section>
-      <section className="home_description">
-        <h1>
-          I'm Luís Azevedo.
-          <br /> A Software Developer
-          <br /> based in Portugal.
-        </h1>
-        <p>lorem ipsum</p>
-      </section>
+      <div className="home_container">
+        <section className="home_picture">
+          <img src={personalPicture} />
+        </section>
+        <section className="home_description">
+          <h1>
+            I'm Luís Azevedo.
+            <br /> A Software Developer
+            <br /> based in Portugal.
+          </h1>
+          <p>lorem ipsum</p>
+        </section>
+      </div>
     </div>
   );
 };
