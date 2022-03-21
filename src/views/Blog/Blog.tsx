4b6687c9050d22f7bb7a7fed2e859c1365b9d6e5
Luís Azevedo <luismiguelgomes2001@gmail.com>
2022-03-21 16:38:12 -0100

    ADD: Contact // Blog

---
+++ src/views/Blog/Blog.tsx
@@ -0,0 +1,14 @@
+import "./Blog.styles.scss";
+
+const Blog = () => {
+  return (
+    <div className="blog">
+      <section className="blog_report">
+        <h1 className="blog_title">Blog</h1>
+        <p>Comming soon...</p>
+      </section>
+    </div>
+  );
+};
+
+export default Blog;
--- src/views/Contact/Contact.tsx
+++ src/views/Contact/Contact.tsx
@@ -1,5 +1,13 @@
+import "./Contact.styles.scss";
 const Contact = () => {
-  return <p>Contact</p>;
+  return (
+    <div className="contact">
+      <section className="contact_container">
+        <h1 className="contact_title">Contact</h1>
+        <p>Call 929320192</p>
+      </section>
+    </div>
+  );
 };
 
 export default Contact;
