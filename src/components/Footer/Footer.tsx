83b142b45d08718096609550bde479b6433e1ca7
Luís Azevedo <luismiguelgomes2001@gmail.com>
2022-03-31 15:16:52 +0000

    ADD: socials/nft/style on Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,28 @@
 import "./Footer.styles.scss";
-import { ReactComponent as SignatureIcon } from "../../assets/signature.svg";
+import NFTPfp from "../../assets/pfp.gif";
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
 
+//TODO: NFT as Logo
 const Footer = () => {
   return (
     <div className="footer-container">
-      <SignatureIcon />
+      <section className="footer-container_copyrights">
+        <header>
+          <img src={NFTPfp} alt="NFT" />
+          <section>
+            <BsLinkedin color="#0077B5" size="24px" />
+            <BsGithub size="24px" />
+          </section>
+        </header>
+        <p>Not Copyright 2022 - Luís Azevedo. </p>
+      </section>
+      <section className="footer-container_nav">
+        <ul>
+          <li>blog</li>
+          <li>contact</li>
+        </ul>
+      </section>
     </div>
   );
 };
