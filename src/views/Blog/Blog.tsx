6dc91e84e2a951c3c407e9ebb610c62441b42990
Luís Azevedo <luismiguelgomes2001@gmail.com>
2022-04-29 21:19:44 +0000

    FIX: style blog report

--- src/views/Blog/Blog.tsx
+++ src/views/Blog/Blog.tsx
@@ -5,7 +5,6 @@ const Blog = () => {
     <div className="blog">
       <section className="blog_report">
         <h1 className="blog_title">COMMING SOON</h1>
-        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn3.iconfinder.com%2Fdata%2Ficons%2Fcommunication-media-malibu-vol-1%2F128%2Fblog-512.png&f=1&nofb=1" />
         <p>The blog is on it's way!</p>
       </section>
     </div>
