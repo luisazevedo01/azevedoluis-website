77ef47e53e6a8f158e835362f7cc420589f45c1a
Luís Azevedo <luismiguelgomes2001@gmail.com>
2022-04-29 21:26:50 +0000

    ADD: report blog icon

--- src/views/Blog/Blog.tsx
+++ src/views/Blog/Blog.tsx
@@ -1,10 +1,11 @@
 import "./Blog.styles.scss";
-
+import { FaRegNewspaper } from "react-icons/fa";
 const Blog = () => {
   return (
     <div className="blog">
       <section className="blog_report">
         <h1 className="blog_title">COMMING SOON</h1>
+        <FaRegNewspaper size="larger" />
         <p>The blog is on it's way!</p>
       </section>
     </div>
