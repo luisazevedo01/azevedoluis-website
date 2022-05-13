2ddf771170c746223dd43168935ed04dec86a6d0
Luís Azevedo <luismiguelgomes2001@gmail.com>
2022-05-13 13:35:31 +0000

    ADD: Collapsible header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { ImCross } from "react-icons/im";
 import { Link, useLocation } from "react-router-dom";
 import { ReactComponent as SignatureIcon } from "../../assets/signature.svg";
@@ -6,32 +6,49 @@ import "./Header.styles.scss";
 
 const Header = () => {
   const location = useLocation();
-  const [active, setActive] = useState("");
+  const [active, setActive] = useState<string>("");
+  const [show, setShow] = useState<boolean>(true);
+  const [prevY, setPrevY] = useState<number>(0);
+
+  const showHeader = () => {
+    window.scrollY > prevY ? setShow(false) : setShow(true);
+    setPrevY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", showHeader);
+  });
 
   useEffect(() => {
     setActive(window.location.href);
   }, [location.pathname]);
 
   return (
-    <div className="header">
-      <Link to="/">
-        <SignatureIcon className="header_signature" />
-      </Link>
-      <ul>
-        <li>
-          {!active.includes("/blog") && (
-            <Link
-              className={
-                active.includes("/blog") ? "header_link-active" : "header_link"
-              }
-              to="/blog"
-            >
-              Blog
-            </Link>
-          )}
-        </li>
-      </ul>
-    </div>
+    <Fragment>
+      {show && (
+        <div className="header">
+          <Link to="/">
+            <SignatureIcon className="header_signature" />
+          </Link>
+          <ul>
+            <li>
+              {!active.includes("/blog") && (
+                <Link
+                  className={
+                    active.includes("/blog")
+                      ? "header_link-active"
+                      : "header_link"
+                  }
+                  to="/blog"
+                >
+                  Blog
+                </Link>
+              )}
+            </li>
+          </ul>
+        </div>
+      )}
+    </Fragment>
   );
 };
 
